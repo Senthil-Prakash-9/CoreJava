ff016ac6702b9370eeb0410b23806b093794c8b6
Senthil-Prakash-9 <154339147+Senthil-Prakash-9@users.noreply.github.com>
2023-12-19 20:16:05 +0530

    Create SwapNumbers.java
    Number Swamp example

---
+++ Basics/src/SwapNumbers.java
@@ -0,0 +1,11 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int x = 9;
+        int y = 3;
+        int temp =x;
+        x = y;
+        y = temp;
+        System.out.println("Number after swapping is "+x+" and "+y);
+
+    }
+}
